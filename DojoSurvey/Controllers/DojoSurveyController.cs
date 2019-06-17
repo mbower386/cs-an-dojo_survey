6204242dfbab24abdbf697bdfd850d1931c3d5dc
mbower386 <mbower386@gmail.com>
2019-06-17 16:04:41 -0700

    finished

--- DojoSurvey/Controllers/DojoSurveyController.cs
+++ DojoSurvey/Controllers/DojoSurveyController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DojoSurvey.Controllers
-{   
+{
     public class DojoSurveyController : Controller
     {
         // Index
@@ -12,5 +12,20 @@ namespace DojoSurvey.Controllers
         {
             return View ();
         }
+
+        // remember to use [HttpPost] attributes!
+        [HttpPost]
+        [Route ("submit")]
+        public IActionResult Submit (string inputName, string location, string language, string comment)
+        {
+            // Do something with form input
+            ViewBag.name = inputName;
+            ViewBag.location = location;
+            ViewBag.language = language;
+            ViewBag.comment = comment;
+
+            return View("Survey");
+        }
+
     }
 }
